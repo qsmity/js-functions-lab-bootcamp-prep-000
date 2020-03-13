0df23d8531859aef3824e6c388bb85146c8b3b02
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-13 13:47:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+
+function happyHolidays(){
+  return "happy holidays!"
+}
+
+var name = "john";
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!` 
+}
+
